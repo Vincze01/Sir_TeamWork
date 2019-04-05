5500e1324cb9fcac2e1e7edcad937cecd07333f0
rskcdy <sajtossajt@gmail.com>
2019-04-05 10:31:40 +0200

    ide gyűjtjük a model basztatási lehetőségeit

---
+++ otletek.R
@@ -0,0 +1,40 @@
+# legyen oltasellenes kozosseg 
+# szuletesek es halalozasok
+# oltas es fertozes folyamatosan verseng: vajon az ujonnan szuletettet sikerul-e beoltani, mielott megfertozodne?
+# aki beteg, nagyobb valoszinuseggel hal meg
+# uj betegseg jelenik meg: aki egyszer beteg volt az egyiktol, az a legyengult szervezete miatt nagyobb esellyel kapja el a masikat
+# aki oltva van az egyik ellen, kisebb esellyel kapja el a masikat
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
