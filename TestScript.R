b0a23da02e0a4ca3909720ad9a1d282e69f4dada
Vincze01 <zsolt.vincze01@gmail.com>
2019-05-06 09:11:44 +0200

    fifo

--- TestScript.R
+++ TestScript.R
@@ -1,3 +1,30 @@
-a = 1
-b = 2
-c = 3
+
+
+newBorn = list()
+timeToVaccination = 5
+
+for(i in 1:21)
+{
+  oldestItem = NULL
+  if(length(newBorn) >= timeToVaccination)
+  {
+    oldestItem = unlist(newBorn[1])
+    newBorn[1] = NULL
+  }
+  newBorn = c(newBorn, i)
+  
+  for (index in 1:length(newBorn))
+  {
+    newBorn[index] = unlist(newBorn[index]) * 2
+  }
+  
+  if(is.null(oldestItem) == FALSE) #Csak ha mar van ilyen elem akkor csinalsz vele barmit
+  {
+    #itt
+  }
+  
+  for (item in newBorn) {
+    print(item)
+  }
+  print("###########################")  
+}
